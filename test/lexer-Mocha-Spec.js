03e42a2376ff87fd766b288214f9f7b0456624d6
clojj <jwin1968@gmail.com>
2015-12-23 11:27:37 +0100

    init

---
+++ test/lexer-Mocha-Spec.js
@@ -0,0 +1,112 @@
+'use strict';
+
+let should = require('chai').should();
+let assert = require('assert');
+
+let lexer = require('../src/lexer');
+
+
+describe('lexer', function () {
+
+    var state = lexer.lex.startState();
+
+    beforeEach(function () {
+    });
+
+    it('is initialized', function () {
+        console.log(lexer.wkw);
+        assert.equal(lexer.wkw.case, 'keyword');
+        assert.deepEqual(lexer.lex.startState(), { f: lexer.normal });
+    });
+
+    it('lex whitespace', function () {
+      var stream = new StringStream("} case abs SomeType 0x12 \"str\"", 2);;
+      var expected = [null, null, 'keyword', null, 'builtin', null, 'variable-2', null, 'integer', null, 'string'];
+
+      let i = 0;
+      let ts = [];
+      while (!stream.eol()) {
+        let token = lexer.lex.token(stream, state);
+
+        ts.push({token: token, pos: stream.pos, value: stream.current()});
+
+        assert.equal(token, expected[i])
+        stream.start = stream.pos;
+        i++;
+      }
+      console.log(JSON.stringify(ts, null, 2));
+    });
+
+});
+
+var StringStream = function(string, tabSize) {
+    this.pos = this.start = 0;
+    this.string = string;
+    this.tabSize = tabSize || 8;
+    this.lastColumnPos = this.lastColumnValue = 0;
+    this.lineStart = 0;
+  };
+
+StringStream.prototype = {
+  eol: function() {return this.pos >= this.string.length;},
+  sol: function() {return this.pos == this.lineStart;},
+  peek: function() {return this.string.charAt(this.pos) || undefined;},
+  next: function() {
+    if (this.pos < this.string.length)
+      return this.string.charAt(this.pos++);
+  },
+  eat: function(match) {
+    var ch = this.string.charAt(this.pos);
+    if (typeof match == "string") var ok = ch == match;
+    else var ok = ch && (match.test ? match.test(ch) : match(ch));
+    if (ok) {++this.pos; return ch;}
+  },
+  eatWhile: function(match) {
+    var start = this.pos;
+    while (this.eat(match)){}
+    return this.pos > start;
+  },
+  eatSpace: function() {
+    var start = this.pos;
+    while (/[\s\u00a0]/.test(this.string.charAt(this.pos))) ++this.pos;
+    return this.pos > start;
+  },
+  skipToEnd: function() {this.pos = this.string.length;},
+  skipTo: function(ch) {
+    var found = this.string.indexOf(ch, this.pos);
+    if (found > -1) {this.pos = found; return true;}
+  },
+  backUp: function(n) {this.pos -= n;},
+  column: function() {
+    if (this.lastColumnPos < this.start) {
+      this.lastColumnValue = countColumn(this.string, this.start, this.tabSize, this.lastColumnPos, this.lastColumnValue);
+      this.lastColumnPos = this.start;
+    }
+    return this.lastColumnValue - (this.lineStart ? countColumn(this.string, this.lineStart, this.tabSize) : 0);
+  },
+  indentation: function() {
+    return countColumn(this.string, null, this.tabSize) -
+      (this.lineStart ? countColumn(this.string, this.lineStart, this.tabSize) : 0);
+  },
+  match: function(pattern, consume, caseInsensitive) {
+    if (typeof pattern == "string") {
+      var cased = function(str) {return caseInsensitive ? str.toLowerCase() : str;};
+      var substr = this.string.substr(this.pos, pattern.length);
+      if (cased(substr) == cased(pattern)) {
+        if (consume !== false) this.pos += pattern.length;
+        return true;
+      }
+    } else {
+      var match = this.string.slice(this.pos).match(pattern);
+      if (match && match.index > 0) return null;
+      if (match && consume !== false) this.pos += match[0].length;
+      return match;
+    }
+  },
+  current: function(){return this.string.slice(this.start, this.pos);},
+  hideFirstChars: function(n, inner) {
+    this.lineStart += n;
+    try { return inner(); }
+    finally { this.lineStart -= n; }
+  }
+};
